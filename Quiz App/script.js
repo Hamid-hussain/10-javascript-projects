61b4d2571d4df5554315307017773ad01c301420
Hamidoo <hussainhafeez1100@gmail.com>
2021-09-21 14:13:05 +0530

    initial commit

---
+++ Quiz App/script.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+    question: 'How old is Florin?',
+    a: '10',
+    b: '15',
+    c: '26',
+    d: '110',
+    correct: 'c'
+  },
+  {
+    question: 'What is the most use best programming Language in 2019?',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'JavaScript',
+    correct: 'd'
+  },
+  {
+    question: 'Who is the President of US?',
+    a: 'Florin Pop',
+    b: 'Donald Trump',
+    c: 'Ivan Saldano',
+    d: 'Mihai Andei',
+    correct: 'b'
+  },
+  {
+    question: 'What does HTML stand for?',
+    a: 'Hypertext Markup Language',
+    b: 'Cascading Style Sheet',
+    c: 'Jason Object Notation',
+    d: 'Helicopters Terminals Motorboats Lamborginis',
+    correct: 'a'
+  },
+  {
+  question: 'What year was JavaScript launched',
+  a: '1996',
+  b: '1995',
+  c: '1994',
+  d: 'none of the above',
+  correct: 'd'
+  }
+];
